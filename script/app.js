0352692e028c0acd4556ab6ce4ff34a2eb36fb81
lungis4ni <lungi.fed@gmail.com>
2021-05-14 19:34:31 +0200

    Working on the mobile Navbar

--- script/app.js
+++ script/app.js
@@ -16,3 +16,32 @@ const menuFunc = () =>
 }
 
 menu.addEventListener('click', menuFunc);
+
+var company = document.getElementById('company');
+var companyUL = document.getElementById('companyUL');
+
+company.addEventListener('click', function()
+{
+    companyUL.classList.toggle("companyUL");
+
+})
+
+var product = document.getElementById('product');
+var productUL = document.getElementById('productUL');
+
+product.addEventListener('click', function()
+{
+    productUL.classList.toggle("productUL");
+
+})
+
+var connect = document.getElementById('connect');
+var connectUL = document.getElementById('connectUL');
+
+connect.addEventListener('click', function()
+{
+    connectUL.classList.toggle("connectUL");
+
+})
+
+console.log(company + " " + companyUL)
